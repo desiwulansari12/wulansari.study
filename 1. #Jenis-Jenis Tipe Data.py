1aa7e967f3346aa64bfe31b83ecf35994b5634af
desiwulansari12 <desi.wulanssari@gmail.com>
2025-03-12 21:56:44 +0700

    menambahakan file baru

---
+++ 1. #Jenis-Jenis Tipe Data.py
@@ -0,0 +1,49 @@
+print ("Jenis-Jenis Tipe Data")
+
+# tipe data Boolean
+print("1. Data boolean = Menyatakan benar True yang bernilai 1, atau salah False yang bernilai 0")
+print ("contoh tipe data boolean")
+print ("benar = True")
+print ("salah = False")
+
+# tipe data String
+print ("2. Data String = Digunakan untuk menyimpan teks atau karakter dalam tanda kutip" )
+print ("contoh tipe data String")
+print("python bahasa yang sangat simpel")
+print('python bahasa favorit saya')
+
+# tipe data String
+print ("3. Data string = Menyatakan bilangan bulat")
+print ("contoh tipe data String")
+print ("bilangan bulat = -10, 0, 10, 20, 2024")
+
+# tipe data float
+print ("4. data float = Menyatakan bilangan yang mempunyai koma")
+print ("contoh tipe data float")
+print ("misalkan 3,19 atau 0.99")
+
+# tipe data hexadecimal
+print ("5. Data hexadecimal = Menyatakan bilangan dalam format heksa, bilangan berbasis 16")
+print ("contoh tipe data hexadecimal")
+print ("misalkan 9a atau 1d3")
+
+# tipe data complex
+print ("6. Data complex = Menyatakan pasangan angka real dan imajiner")
+print (" contoh tipe data complex")
+print ('misalkan 1 + 5j')
+
+# tipe data List
+print ("7. Data List = Struktur data yang bisa menyimpan banyak elemen dan dapat diubah (mutable)")
+print (" contoh tipe data List")
+print ("contoh List : 1, 2, 3, 4, 5")
+print ("Setelah menambah elemen: 1, 2, 3, 4, 5, 6")
+
+# tipe data Tuple
+print ("8. Data tuple = Mirip dengan list tetapi tidak dapat diubah (immutable)")
+print (" contoh tipe data Tuple")
+print ("contoh List : 1, 2, 3, 4, 5")
+
+# tipe data Dictionary
+print ("9. Data Dictionary = Data untaian yang menyimpan berbagai tipe data berupa pasangan penunjuk dan nilai")
+print ("contoh tipe data Dictionary")
+print ("contoh Dictionary : {'nama': 'wulan', 'umur': 19, 'jurusan': 'Teknik Informatika'}")
